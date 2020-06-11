4c9ffb69cb18bdc94e5a70f271924a3c3e3ec55c
Emperor <21046314+empdarkness@users.noreply.github.com>
2020-06-10 20:51:06 -0400

    Update config.py
    Typo

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ token = ""
 ### guild (id) to get roles from
 guildid =
 # role format needs to be specific
-# Factions: Grineer, Infested, Corpus, Infested
+# Factions: Grineer, Infested, Corpus, Orokin
 # Types: Grineer Defense, Corpus Survival..
 # Nodes: Sechura, Helene, Hydron...
 ## webhook url to post current arbitration
